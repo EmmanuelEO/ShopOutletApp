016547a05d41f082616749b62548a629d30d18b2
emmanuel_okonkwo <chukwujekwuekene2017@gmail.com>
2022-01-06 22:59:30 -0800

    Made some changes

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 
 const Search = () => {
-    const []
+  const navigate = useNavigate()
+  const [keyword, setKeyword] = useState('')
 
-    return (
-        <div>
-            
-        </div>
-    )
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (keyword.trim()) {
+      navigate(`/search/${keyword}`)
+    } else {
+      navigate('/')
+    }
+  }
+
+  return (
+    <Form className='py-3' onSubmit={submitHandler} inline='true'>
+      <div className='input-group'>
+        <Form.Control
+          type='text'
+          name='theForm'
+          onChange={(e) => setKeyword(e.target.value)}
+          placeholder='Search for products...'
+          className='mr-sm-2 ml-sm-5'
+        ></Form.Control>
+        <Button type='submit' variant='outline-success' className='p-2'>
+          Search
+        </Button>
+      </div>
+    </Form>
+  )
 }
 
 export default Search
